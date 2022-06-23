9927a80d1ed6c49606855c0d89e43b13ac8cfbdd
najeebaslan <najeebaslan2019@gmail.com>
2022-06-23 17:18:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# issue
+
+# App Description
+
+### This application allows you to add the accused and manage all his data in terms of modification, deletion, etc. It shows alerts when the time available for the accused expires and the time specified for alerting is the first week from the date of adding the accused, then after forty-five days, then also after another forty-five days.
+
---
+++ lib/services/manage_function_background/terminated_app.dart
@@ -0,0 +1,20 @@
+//version 1.0.0
+import 'dart:developer';
+import 'utils/local_notification.dart';
+
+class AppTerminated {
+ static  Future<void> whineAppIsTerminated(
+      {required int id,
+      required String accuseName,
+      required String title,
+      }) async {
+  await  WhineLocalNotification.localNotification(
+      id: id,
+      name: accuseName,
+      title: title,
+      dateTime: DateTime.now(),
+    );
+  }
+}
+
+
